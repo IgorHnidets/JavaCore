2aea41888c3c18f1a628aa5b59a48f3d73c4595f
IgorHnidets <igora6669@gmail.com>
2022-04-28 23:35:03 +0300

    Commit1

---
+++ src/com/Lesson11_Interator_Comparator/Classwork/Product.java
@@ -0,0 +1,105 @@
+package com.Lesson11;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Product implements Comparable<Product> {
+
+    private String name;
+    private double price;
+    private long idcode;
+    private String departament;
+
+    public Product(String name, double price, long idcode, String departament) {
+        this.name = name;
+        this.price = price;
+        this.idcode = idcode;
+        this.departament = departament;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getIdcode() {
+        return idcode;
+    }
+
+    public void setIdcode(long idcode) {
+        this.idcode = idcode;
+    }
+
+    public String getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(String departament) {
+        this.departament = departament;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return
+        return Double.compare(product.price, price) == 0 && idcode == product.idcode && Objects.equals(name, product.name) && Objects.equals(departament, product.departament);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, idcode, departament);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", idcode=" + idcode +
+                ", departament='" + departament + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Product p) {
+        int result = this.name.compareTo(p.name);
+        if (result == 0) {
+            result = (int)(this.price - p.price);
+
+        }
+        return result;
+    }
+
+    public static Comparator<Product> departmentC() {
+        Comparator<Product> departmentC = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o2.getDepartament().compareTo(o1.getDepartament());
+            }
+        }
+        return departmentC;
+    }
+    public static  Comparator<Product> priceC() {
+        Comparator<Product> priceC = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return (int) (o1.getPrice() - o2.getPrice());
+            }
+        };
+        return priceC;
+    }
+
+}
